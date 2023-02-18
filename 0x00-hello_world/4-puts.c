ea29aed0470ae5decf7f38f69c0a68023428504e
Bimijay <blesynjackson@gmail.com>
2023-02-17 16:53:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@ int main(void)
 *return: always 0 (success)
 */
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+          puts("\"Programming is like building a multilingual puzzle");
+          return (0);
 }	
 
 
