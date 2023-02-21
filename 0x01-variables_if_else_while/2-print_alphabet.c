ec791f8985af8cf38db101afb809e8de6037e2ff
Bimijay <blesynjackson@gmail.com>
2023-02-20 17:19:35 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - is entry of program
+ * main - Prints the alphabet.
  *
- * Return: 0, if succesful
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/*
-	 * prints all combination of 2 different digits once,in accending order
-	 * after each combination follows a coma and a space
-	 */
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	/* take ab as possible digits */
-	int a, b;
-
-	/*loop through a,1st digit,lowest of the 2, highest possible is 8*/
-	for (a = 0 ; a <= 8 ; a++)
+	for (i = 0; i < 26; i++)
 	{
-		/*loop through b,2nd digit,higher than a by 1,highest possible is 9*/
-		for (b = a + 1 ; b <= 9 ; b++)
-		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
-			/*continues to put coma,space,stop if last highest combination is met*/
-			if (a == 8 && b == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
