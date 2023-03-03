3c5fd7875c2aebf91f5bd46467f3d341a6f032f7
Bimijay <blesynjackson@gmail.com>
2023-03-02 17:07:35 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 * main - check the code for
 *
@@ -8,11 +7,10 @@
 */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-	char *p;
-
-	p = leet;
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
+char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+char *p;
+p = leets;
+printf("%s", p);
+printf("%s", s);
+return (0);
 }
